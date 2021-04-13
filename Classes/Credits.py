6c1f40e9f74c8d93e8816ba0c4915f3ad270acf3
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2021-04-13 09:28:44 +0700

    Add files via upload

---
+++ Classes/Credits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__version__ = '2.6'
+__author__ = 'O.Koleda (real author)'
+__improver__= 'mIcHy AmRaNe & Xnuvers007'
+
+def getCredits():
+
+    return '''
+     ____  __ __          __          _       _______           __
+    / __ \/ //_/___ _____/ /___ ___  (_)___  / ____(_)___  ____/ /__  _____
+   / / / / ,< / __ `/ __  / __ `__ \/ / __ \/ /_  / / __ \/ __  / _ \/ ___/
+  / /_/ / /| / /_/ / /_/ / / / / / / / / / / __/ / / / / / /_/ /  __/ /
+  \____/_/ |_\__,_/\__,_/_/ /_/ /_/_/_/ /_/_/   /_/_/ /_/\__,_/\___/_/
+     version %s created by %s & rewritten by %s
+
+    ''' % (__version__, __author__, __improver__,), __version__
